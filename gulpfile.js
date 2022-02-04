92963ecc05b56bf3e5f09060fe7c5f92844d9aaa
hisashin <shingohisakawa@gmail.com>
2022-02-05 02:49:10 +0900

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,16 @@ const typescript = require('gulp-typescript');
 const sass = require('gulp-sass');
 const exec = require('gulp-exec');
 
-const doPug = () => {
+const doPug = (done) => {
   gulp.src(['${__dirname}/pug/**/*.pug', '${__dirname}/pug/**/*.jade', '!**/layout*', '!**/include/*', '!**/includes/*'])
   .pipe(pug({
     verbose: true,
     pretty: true
   }))
   .pipe(gulp.dest('${__dirname}/production/'));
+  done();
 };
-const doPugI18n = () => {
+const doPugI18n = (done) => {
   var options = {
     i18n: {
       verbose: true,
@@ -31,22 +32,25 @@ const doPugI18n = () => {
   gulp.src(['${__dirname}/pugI18n/**/*.pug', '${__dirname}/pugI18n/**/*.jade', '!**/layout*', '!**/include/*', '!**/includes/*'])
   .pipe(pugI18n(options))
   .pipe(gulp.dest(options.i18n.dest));
+  done();
 };
 
-const doSass = () => {
+const doSass = (done) => {
   gulp.src('${__dirname}/scss/**/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('${__dirname}/production/'));
+  done();
 };
 
-const doTs = () => {
+const doTs = (done) => {
   gulp.src([ '${__dirname}/ts/**/*.ts' ])
   .pipe(typescript({ target: 'ES5', module: 'commonjs' }))
   .js
   .pipe(gulp.dest('${__dirname}/production/'));
+  done();
 };
 
-const doShell = () => {
+const doShell = (done) => {
   gulp.src('.')
   .pipe(exec("${__dirname}/build_local_console.sh",(err, stdout, stderr) => {
     if (stdout) {
@@ -56,12 +60,14 @@ const doShell = () => {
       console.log(stderr);
     }
   }));
+  done();
 };
 /*
-const doBabel = () => {
+const doBabel = (done) => {
   gulp.src('${__dirname}/production/console/js-ES6/*.js')
     .pipe(babel())
     .pipe(gulp.dest('${__dirname}/production/console/js'))
+  done();
 };
 */
 exports.default = gulp.parallel(doPug, doPugI18n);
